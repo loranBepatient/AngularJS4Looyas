cd6f1fd5d9a013bf89d5394ea3fdc449528bc5fb
lo <laurent.muller@bepatient.com>
2020-11-01 00:50:00 +0100

    master: Updated move to service

--- app/todos/todos.component.js
+++ app/todos/todos.component.js
@@ -3,80 +3,21 @@ var component = {
   controller: controller,
 };
 
-function controller($http) {
+function controller(todosService) {
   var ctrl = this;
-  ctrl.counter = 0;
+  ctrl.todos = [];
+
   ctrl.$onInit = onInit;
+
   ctrl.addOne = addOne;
   ctrl.substractOne = substractOne;
   ctrl.markCompleted = markCompleted;
   ctrl.deleteTodo = deleteTodo;
 
-  ctrl.todos = [
-    {
-      userId: 1,
-      id: 1,
-      title: "delectus aut autem",
-      completed: false,
-    },
-    {
-      userId: 1,
-      id: 2,
-      title: "quis ut nam facilis et officia qui",
-      completed: false,
-    },
-    {
-      userId: 1,
-      id: 3,
-      title: "fugiat veniam minus",
-      completed: false,
-    },
-    {
-      userId: 1,
-      id: 4,
-      title: "et porro tempora",
-      completed: true,
-    },
-    {
-      userId: 1,
-      id: 5,
-      title: "laboriosam mollitia et enim quasi adipisci quia provident illum",
-      completed: false,
-    },
-    {
-      userId: 1,
-      id: 6,
-      title: "qui ullam ratione quibusdam voluptatem quia omnis",
-      completed: false,
-    },
-    {
-      userId: 1,
-      id: 7,
-      title: "illo expedita consequatur quia in",
-      completed: false,
-    },
-    {
-      userId: 1,
-      id: 8,
-      title: "quo adipisci enim quam ut ab",
-      completed: true,
-    },
-  ];
-
   function onInit() {
-    getTodosList();
-  }
-
-  function getTodosList() {
-    var url = "https://jsonplaceholder.typicode.com/todos";
-    $http
-      .get(url)
-      .then(function (todos) {
-        ctrl.todos = todos.data;
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    todosService.getTodos().then(function (todos) {
+      ctrl.todos = todos;
+    });
   }
 
   function addOne() {
---
+++ app/todos/todos.service.js
@@ -0,0 +1,21 @@
+angular.module("mySuperApp").factory("todosService", todosService);
+
+function todosService($http) {
+  var url = "https://jsonplaceholder.typicode.com/todos";
+
+  return {
+    getTodos: getTodos,
+  };
+
+  function getTodos() {
+    return $http.get(url).then(getTodosComplete).catch(getTodosFailed);
+
+    function getTodosComplete(response) {
+      return response.data;
+    }
+
+    function getTodosFailed(error) {
+      return error.message;
+    }
+  }
+}
